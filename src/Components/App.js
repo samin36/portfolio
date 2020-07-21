93811f9f3c1bbb082f8871d6bd00babc7a7586a0
Shrey Amin <samin36@gatech.edu>
2020-07-21 18:54:17 -0400

    auto update

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import RouterSetup from "./RouterSetup";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const App = () => {
   return (
